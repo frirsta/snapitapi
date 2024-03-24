3dc02054048897855943a017d1f544a069671c85
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-03-24 07:28:44 +0100

    fix

--- api/settings.py
+++ api/settings.py
@@ -35,7 +35,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         #  'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
         # 'rest_framework.authentication.SessionAuthentication',
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
 
     ],

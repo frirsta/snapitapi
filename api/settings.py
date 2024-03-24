b2dd0c315060e7a4c2557283ace01b8cd0d8993a
frirsta <frirsta.karem@hantverkshjalpenonline.se>
2024-03-24 07:03:41 +0100

    Fix

--- api/settings.py
+++ api/settings.py
@@ -34,7 +34,7 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_AUTHENTICATION_CLASSES': [
         #  'dj_rest_auth.jwt_auth.JWTCookieAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
         'dj_rest_auth.jwt_auth.JWTCookieAuthentication'
 
